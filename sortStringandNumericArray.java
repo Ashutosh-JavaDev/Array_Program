98716532797b094a7aa9244afa9219dad0f67992
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-05 21:56:09 +0530

    First Array Commit

--- sortStringandNumericArray.java
+++ sortStringandNumericArray.java
@@ -1,5 +1,20 @@
 package Array_Program;
 import java.util.*;
 public class sortStringandNumericArray{
-    
+    public static String Result(int arr[],int size){
+        String answer=Arrays.toString(arr);
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter your Value");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        String output=Result(arr, size);
+        System.out.println("Answer: "+output);
+    }
 }
