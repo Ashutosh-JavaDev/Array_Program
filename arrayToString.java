8a7eef23dcc5806978e5503a25feb1a2906ef008
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-05 22:06:23 +0530

    First Array Commit

--- arrayToString.java
+++ arrayToString.java
@@ -1,20 +1,27 @@
 package Array_Program;
+
 import java.util.*;
-public class arrayToString{
-    public static String Result(int arr[],int size){
-        String answer=Arrays.toString(arr);
-        return answer;
+
+public class arrayToString {
+    public static String Result(int arr[], int size) {
+        String answer = Arrays.toString(arr);
+        System.out.println("Original Array: "+answer);
+        Arrays.sort(arr);
+        String value = Arrays.toString(arr);
+        return value;
+
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter your Value");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        String output=Result(arr, size);
-        System.out.println("Answer: "+output);
+        String output = Result(arr, size);
+        System.out.println("Sorted Value : " + output);
     }
 }
