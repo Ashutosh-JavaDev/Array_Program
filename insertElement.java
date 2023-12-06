baa46c7dd73ac42abd615fb359777d682b75381a
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-06 16:36:27 +0530

    Array Commit

--- insertElement.java
+++ insertElement.java
@@ -1,24 +1,24 @@
-//Write a Java program to insert an element (specific position) into an array.
 package Array_Program;
 
 import java.util.*;
 
 public class insertElement {
     public static void result(int arr[], int index, int size, int number) {
-        if (index <= size) {
-            for (int i = index; i < size; i++) {
-                arr[i] = arr[i + 1];
+        if (index >= 0 && index <= size) {
+            // Shift elements to the right to make space for the new element
+            for (int i = size - 1; i > index; i--) {
+                arr[i] = arr[i - 1];
             }
+            // Insert the new element at the specified index
             arr[index] = number;
+
             System.out.println("New Array");
             for (int i = 0; i < size + 1; i++) {
                 System.out.println(arr[i]);
             }
-
         } else {
-            System.out.println("Invalid Index:Index Size is more than original size.");
+            System.out.println("Invalid Index: Index should be between 0 and " + size);
         }
-
     }
 
     public static void main(String[] args) {
@@ -26,13 +26,13 @@ public class insertElement {
         System.out.println("Enter the size of the array");
         int size = sc.nextInt();
         int arr[] = new int[size];
-        System.out.println("Enter the value");
+        System.out.println("Enter the values");
         for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
-        System.out.println("Enter the Number want to add");
+        System.out.println("Enter the Number you want to add");
         int number = sc.nextInt();
-        System.out.println("Enter the index wheere the number wants to add ");
+        System.out.println("Enter the index where the number should be added");
         int index = sc.nextInt();
         result(arr, index, size, number);
     }
