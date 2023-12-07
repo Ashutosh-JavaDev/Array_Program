e5b937968c6d815c34966d7e777975a2fe2f576f
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-07 21:31:08 +0530

    Array Commit

--- differenceBetweenLargestandSmallest.java
+++ differenceBetweenLargestandSmallest.java
@@ -4,7 +4,7 @@ package Array_Program;
 import java.util.*;
 
 public class differenceBetweenLargestandSmallest {
-    public static void Difference(int arr[], int size) {
+    public static int Difference(int arr[], int size) {
         int max = arr[0];
         int secmax = arr[1];
         for (int i = 0; i < size; i++) {
@@ -13,5 +13,21 @@ public class differenceBetweenLargestandSmallest {
                 max = arr[i];
             }
         }
+        System.out.println("Max Number:"+max);
+        System.out.println("Second Max:"+secmax);
+        int difference=max-secmax;
+        return difference;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the value nin the array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int answer=Difference(arr, size);
+        System.out.println("Difference between Maximun and Second Maximun Number:"+answer);
     }
 }
