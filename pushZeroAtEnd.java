f01dfa5e8e53116f01e1196f5d9cadb099d5f10b
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-07 17:44:45 +0530

    Array Commit

--- pushZeroAtEnd.java
+++ pushZeroAtEnd.java
@@ -4,32 +4,32 @@ import java.util.*;
 
 public class pushZeroAtEnd {
     public static void nonZero(int arr[], int size) {
+        int nonzeroIndex = 0;
+
         for (int i = 0; i < size; i++) {
             if (arr[i] != 0) {
-                System.out.println(arr[i]);
+                int temp = arr[i];
+                arr[i] = arr[nonzeroIndex];
+                arr[nonzeroIndex] = temp;
+                nonzeroIndex++;
             }
+        }
+        System.out.println("Seperating Zero and NonZero");
+        for(int i=0;i<size;i++){
+            System.out.println(arr[i]);
         }
     }
 
-    public static void zero(int arr[], int size) {
-        for (int i = 0; i < size; i++) {
-            if (arr[i] == 0) {
-                System.out.println(arr[i]);
-            }
-        }
-    }
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the value int the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        System.out.println("Seperating Zero and NonZero");
         nonZero(arr, size);
-        zero(arr, size);
 
     }
 }
