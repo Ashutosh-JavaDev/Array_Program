5a5092e7ada765d9a9e9aaf183d78424a4c294b4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 20:50:13 +0530

    Array Commit

--- suffleArray.java
+++ suffleArray.java
@@ -6,11 +6,9 @@ public class suffleArray {
         int index=0;
         for(int i=0;i<size;i++){
            for(int j=i+1;j<size;j++){
-            if(arr[i]<arr[j]){
                 int temp=arr[i];
                 arr[i]=arr[j];
                 arr[j]=temp;
-            }
            }
         }
         System.out.println("Shuffle Array:");
