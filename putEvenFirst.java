8b6008260476db94730fb26bd9c24e169a8ba626
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 16:05:11 +0530

    Array Commit

---
+++ putEvenFirst.java
@@ -0,0 +1,8 @@
+//Write a Java program to separate even and odd numbers from a given array of integers. Put all even numbers first, and then odd numbers. 
+package Array_Program;
+import java.util.*;
+public class putEvenFirst {
+    public static void EvenFirst(int arr[],int size){
+        
+    }
+}
