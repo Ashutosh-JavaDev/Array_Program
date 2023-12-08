93116c4313d29893f280ce6d6fc37abb5b1ea45a
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 16:57:00 +0530

    Array Commit

--- SubArray.java
+++ SubArray.java
@@ -2,7 +2,30 @@
 package Array_Program;
 import java.util.*;
 public class SubArray {
-    public static void subArray(int arr[],int size){
-        
+    public static boolean subArray(int arr[],int size){
+        boolean result=true;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if((arr[i]+arr[i])==0){
+                    result=true;
+                }
+                else{
+                    result=false;
+                }
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the Posetive and negative Number to check the program");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        boolean answer=subArray(arr, size);
+        System.out.println("Does the said array contain a subarray with 0 sum:"+answer);
     }
 }
