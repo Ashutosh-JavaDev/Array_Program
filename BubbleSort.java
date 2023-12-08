f7707fb526d971a4ab8b9a1438cf1083fd272f90
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 18:27:58 +0530

    Array Commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
     public static void sorting(int arr[], int size) {
         for (int i = 0; i < size; i++) {
             for (int j = i + 1; j < size - i - 1; j++) {
-                if (arr[i] < arr[j]) {
+                if (arr[i] > arr[i+1]) {
                     int temp = arr[i];
                     arr[i] = arr[i + 1];
                     arr[i + 1] = temp;
