e2f860241bdef1a23368cc84579431a5f845f3bf
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 17:00:39 +0530

    Array Commit

--- SubArray.java
+++ SubArray.java
@@ -1,31 +1,34 @@
-//Write a Java program to check if a given array contains a subarray with 0 sum. 
 package Array_Program;
-import java.util.*;
+
+import java.util.Scanner;
+
 public class SubArray {
-    public static boolean subArray(int arr[],int size){
-        boolean result=true;
-        for(int i=0;i<size;i++){
-            for(int j=i+1;j<size;j++){
-                if((arr[i]+arr[i])==0){
-                    result=true;
+    public static boolean subArray(int arr[], int size) {
+        boolean result = false;
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size; j++) {
+                if ((arr[i] + arr[j]) == 0) {
+                    result = true;
+                    break; // Added break to exit the inner loop once a pair is found
                 }
-                else{
-                    result=false;
-                }
+            }
+            if (result) {
+                break; // Added break to exit the outer loop once a pair is found
             }
         }
         return result;
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        System.out.println("Enter the Posetive and negative Number to check the program");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the Positive and negative Numbers to check the program");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
-        boolean answer=subArray(arr, size);
-        System.out.println("Does the said array contain a subarray with 0 sum:"+answer);
+        boolean answer = subArray(arr, size);
+        System.out.println("Does the said array contain a subarray with 0 sum: " + answer);
     }
 }
