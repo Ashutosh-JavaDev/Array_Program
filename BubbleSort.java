026718cd0d673c07b499345a96e9dd3bf390c6a4
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 18:26:35 +0530

    Array Commit

---
+++ BubbleSort.java
@@ -0,0 +1,34 @@
+package Array_Program;
+
+import java.util.*;
+
+public class BubbleSort {
+    public static void sorting(int arr[], int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = i + 1; j < size - i - 1; j++) {
+                if (arr[i] < arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+            }
+        }
+        System.out.println("Array After Sorting");
+        for (int i = 0; i < size; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the sizee of the Array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the value in the Array");
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sorting(arr, size);
+    }
+}
