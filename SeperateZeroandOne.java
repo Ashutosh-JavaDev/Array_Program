e4fc49bbbe97137c27c2db0c0b6576ae1fcbc07e
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 14:08:20 +0530

    Array Commit

---
+++ SeperateZeroandOne.java
@@ -0,0 +1,8 @@
+//Write a Java program to separate 0s and 1s in an array of 0s and 1s into left and right sides.  
+package Array_Program;
+import java.util.*;
+public class SeperateZeroandOne {
+    public static void Seperate(int arr[],int size){
+        
+    }
+}
