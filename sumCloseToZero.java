0461cae876ac4baf36cecc9a6650b8b56231d6c3
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-08 13:57:26 +0530

    Java Program

--- sumCloseToZero.java
+++ sumCloseToZero.java
@@ -23,4 +23,15 @@ public class sumCloseToZero {
             System.out.println("Answer is far from zero");
         }
     }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        check(arr, size);
+    }
 }
