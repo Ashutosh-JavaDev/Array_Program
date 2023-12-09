4bef5d78dba68ffeb5e39a1934795ea28122970f
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-09 20:57:00 +0530

    Array Commit

--- nextProduct.java
+++ nextProduct.java
@@ -5,25 +5,37 @@ import java.util.*;
 
 public class nextProduct {
     public static void productNext(int arr[], int size) {
-        int product=1;
-        for(int i=0;i<size;i++){
-            product=product*arr[i];
+        int totalProduct = 1;
+
+        // Calculate the product of all elements
+        for (int i = 0; i < size; i++) {
+            totalProduct *= arr[i];
         }
+
         System.out.println("The Product after next Number");
-        for(int i=0;i<size;i++){
-            System.out.print(product/arr[i]+" ");
+
+        // Print the product of every other element
+        for (int i = 0; i < size; i++) {
+            System.out.print(totalProduct / arr[i] + " ");
         }
+
         System.out.println();
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter the size of the Array");
-        int size=sc.nextInt();
-        int arr[]=new int[size];
-        System.out.println("Enter the value in the Array");
-        for(int i=0;i<size;i++){
-            arr[i]=sc.nextInt();
+        int size = sc.nextInt();
+
+        int arr[] = new int[size];
+
+        System.out.println("Enter the values in the Array");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
         }
+
         productNext(arr, size);
     }
 }
+
