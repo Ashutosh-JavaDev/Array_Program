5ed5679fe0718ef48a34e56d5c2861a1f16567cf
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-09 20:50:25 +0530

    Array Commit

--- nextProduct.java
+++ nextProduct.java
@@ -11,7 +11,7 @@ public class nextProduct {
         }
         System.out.println("The Product after next Number");
         for(int i=0;i<size;i++){
-            System.out.print(product/arr[i]);
+            System.out.print(product/arr[i]+" ");
         }
     }
     public static void main(String[] args) {
