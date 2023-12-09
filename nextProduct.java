b36454560f18bf08f7fd06a8586e4ce29bb71a10
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-09 20:47:15 +0530

    Array Commit

--- nextProduct.java
+++ nextProduct.java
@@ -5,6 +5,16 @@ import java.util.*;
 
 public class nextProduct {
     public static void productNext(int arr[], int size) {
-
+        int product=1;
+        for(int i=0;i<size;i++){
+            product=product*i;
+        }
+        System.out.println("The Product after next Number");
+        for(int i=0;i<size;i++){
+            System.out.print(product/arr[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
     }
 }
