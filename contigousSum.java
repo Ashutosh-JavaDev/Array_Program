71038ab478323f8e0b1a29db25e688ae98604129
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-09 21:35:56 +0530

    Array Commit

--- contigousSum.java
+++ contigousSum.java
@@ -6,13 +6,26 @@ public class contigousSum {
         int current_Sum=0;
         int max_Sum=0;
         for(int i=0;i<size;i++){
+            current_Sum+=arr[i];
             if(current_Sum>max_Sum){
                 max_Sum=current_Sum;
             }
-            else{
+            if(current_Sum<0){
                 current_Sum=0;
             }
         }
         return max_Sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        int output=Contigous(arr, size);
+        System.out.println("Contigous Array Sum:"+output);
     }
 }
