b9eecc3b8398c3ab3e4d25db927ea046a8ef0283
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-09 20:49:01 +0530

    Array Commit

--- nextProduct.java
+++ nextProduct.java
@@ -16,5 +16,13 @@ public class nextProduct {
     }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the value in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        productNext(arr, size);
     }
 }
