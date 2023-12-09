ad2c90373d81483d2c8219d22cba68de2f581f67
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-09 20:43:19 +0530

    Array Commit

--- nextProduct.java
+++ nextProduct.java
@@ -1,6 +1,10 @@
 //Write a Java program to replace each element of the array with the product of every other element in a given array of integers.  
 package Array_Program;
 
+import java.util.*;
+
 public class nextProduct {
-    
+    public static void productNext(int arr[], int size) {
+
+    }
 }
