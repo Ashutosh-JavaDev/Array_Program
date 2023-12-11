99e7c7a57955eaf39c78ebca015e4ed2e1fdb4dd
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-11 09:56:35 +0530

    Array Commit

--- missingNumber.java
+++ missingNumber.java
@@ -13,11 +13,17 @@ public class missingNumber {
         int newNumber = temp - sum;
         return newNumber;
     }
+
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of the Array");
-        int  size=sc.nextInt();
-        int arr[]=new int[size];
+        int size = sc.nextInt();
+        int arr[] = new int[size];
         System.out.println("Enter the Value in the Array");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+
+        }
+
     }
 }
