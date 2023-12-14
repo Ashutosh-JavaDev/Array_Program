fcaa95372ed3b0f3efe80b670821f44e3b8909a5
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-14 11:31:25 +0530

    Java Development

--- PrimeMatrix.java
+++ PrimeMatrix.java
@@ -3,6 +3,20 @@ import java.util.*;
 public class PrimeMatrix {
     public static void main(String[] args){
         Scanner  sc=new Scanner(System.in);
-        
+        System.out.println("Enter the Size of the Array");
+        int size=sc.nextInt();
+        int arr[][]=new int[size][size]; 
+        int count=0;
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size;j++){
+                if(arr[i][j]%i==0){
+                    count++;
+                }
+                if(count==2){
+                    System.out.print(arr[i][j]);
+                }
+            }
+            System.out.println();
+        }
     }
 }
