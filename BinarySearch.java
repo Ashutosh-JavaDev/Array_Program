55c4a33419be287e8cdd55f8e27c1d6ed0290f9d
Ashutosh Ayush <jbnashu297@gmail.com>
2023-12-15 09:15:19 +0530

    Java Development

---
+++ BinarySearch.java
@@ -0,0 +1,10 @@
+package Array_Program;
+import java.util.*;
+public class BinarySearch {
+    public static void PrintSearch(int arr[],int size,int num){
+        int first=0,mid=0,last=size-1,count=0;
+        while(first<last){
+            
+        }
+    }    
+}
