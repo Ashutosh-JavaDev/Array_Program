efbb4660364f93c402dd48fa6b5d775e822869b2
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-10 15:14:48 +0530

    Java progam

--- cubicArray.java
+++ cubicArray.java
@@ -1,5 +1,17 @@
 package Array_Program;
 import java.util.Scanner;
+class cubeAr{
+    int arr[];
+    int[] cube(){
+        Scanner sc=new Scanner(System.in);
+        arr=new int[10];
+        System.out.println("Enter the Value in the Array");
+        for(int i=0;i<10;i++){
+            arr[i]=sc.nextInt();
+        }
+
+    }
+}
 public class cubicArray {
     
 }
