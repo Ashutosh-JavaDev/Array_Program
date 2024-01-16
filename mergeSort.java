0ad0fd39eeebc278989d784d9aa12e0452435cd7
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-16 10:24:09 +0530

    Java Program

--- mergeSort.java
+++ mergeSort.java
@@ -3,57 +3,69 @@ package Array_Program;
 import java.util.Scanner;
 
 class merge {
-    void sorting(int arr[], int s,int mid, int e) {
-         mid = s + (e - s) / 2;
-        int j = mid + 1, i = s,k=0;
-        // int k[]=new int[e+1];
-        while (i <= mid && j <= e) {
-            if (arr[i] > arr[j]) {
-                arr[k] = arr[i];
+    void sorting(int arr[], int s, int mid, int e) {
+       mid=s+(e-s)/2;
+       int arr1[]=new int[mid+1];
+        int arr2[]=new int[mid];
+        int n1=mid+1;
+        int n2=mid;
+        for(int i=0;i<n1;i++){
+            arr1[i]=arr[s];
+            s++;
+        }
+        for(int i=0;i<n2;i++){
+            arr2[i]=arr[n1];
+            n1++;
+        }
+        int i=0,j=0,k=0;
+        while (i < n1 && j < n2) {
+            if (arr1[i] > arr2[j]) {
+                arr[k] = arr1[i];
                 i++;
                 k++;
             } else {
-                arr[k] = arr[j];
+                arr[k] = arr2[j];
                 j++;
                 k++;
             }
         }
-        while (i < mid) {
-            arr[k] = arr[i];
+        while (i < n1) {
+            arr[k] = arr1[i];
             i++;
             k++;
         }
-        while (j < e) {
-            arr[k] = arr[j];
+        while (j < n2) {
+            arr[k] = arr2[j];
             j++;
             k++;
         }
-        System.out.println("Array After sorting");
-        for(int l=0;l<e;l++){
-            System.out.println(arr[l]);
-        }
+
     }
-    void mergesort(int arr[],int s,int e){
-        if(s<e){
-            int mid=s+(e+s)/2;
-            mergesort(arr, mid+1, e);
-            sorting(arr, s,mid, e);
+
+    void mergesort(int arr[], int s, int e) {
+        if (s < e) {
+            int mid = s + (e + s) / 2;
+            mergesort(arr, mid + 1, e);
+            sorting(arr, s, mid, e);
         }
     }
 }
 
 public class mergeSort {
-public static void main(String[] args) {
-    Scanner sc=new Scanner(System.in);
-    System.out.println("Enter the Size of the Array");
-    int size=sc.nextInt();
-    int arr[]=new int[size];
-    System.out.println("Enter the Value in the Array");
-    for(int i=0;i<size;i++){
-        arr[i]=sc.nextInt();
-    }    
-    merge ob=new merge();
-    ob.mergesort(arr, 0, size-1);
-
-}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of the Array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter the Value in the Array");
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+        merge ob = new merge();
+        ob.mergesort(arr, 0, size - 1);
+        System.out.println("Array After sorting");
+        for (int l = 0; l < size; l++) {
+            System.out.println(arr[l]);
+        }
+    }
 }
