49838157f47fb292190f990bb200cb3c6028240b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-16 13:30:50 +0530

    Java Program

---
+++ Merge.java
@@ -0,0 +1,15 @@
+package Array_Program;
+import java.util.Scanner;
+class sorting{
+    void mergesort(int arr[],int s,int mid,int e){
+        int arr1[]=new int[e-s+1];
+        mid=s+(e-s)/2;
+        int i=s,k=0,j=mid+1;
+        while(i<=mid && j<=e){
+            
+        }
+    }
+}
+public class Merge {
+    
+}
