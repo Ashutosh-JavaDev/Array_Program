7a198d4ef526cb409648a39b488225bd7c9f0324
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-16 09:32:41 +0530

    Java Program

--- mergeSort.java
+++ mergeSort.java
@@ -3,8 +3,8 @@ package Array_Program;
 import java.util.Scanner;
 
 class merge {
-    void sorting(int arr[], int s, int e) {
-        int mid = s + (e - s) / 2;
+    void sorting(int arr[], int s,int mid, int e) {
+         mid = s + (e - s) / 2;
         int j = mid + 1, i = s, k = 0;
         // int k[]=new int[e+1];
         while (i <= mid && j <= e) {
@@ -27,6 +27,13 @@ class merge {
             arr[k] = arr[j];
             j++;
             k++;
+        }
+    }
+    void mergesort(int arr[],int s,int e){
+        if(s<e){
+            int mid=s+(e+s)/2;
+            mergesort(arr, mid+1, e);
+            sorting(arr, s,mid, e);
         }
     }
 }
