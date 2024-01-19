b7ee54d237bfe9f7792dedd7fa82bbef9b16c4fb
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 00:28:03 +0530

    Merge Sort

--- DifferentSorting/MergeSort.java
+++ DifferentSorting/MergeSort.java
@@ -1,37 +1,65 @@
 package Array_Program.DifferentSorting;
+
 import java.util.Scanner;
-class mergeSort{
-    void logicMerge(int arr[],int s,int mid,int e){
-        mid=s+(e-s)/2;
-        int i=0,j=mid+1,k=0;
-        int merge[]=new int[e-s+1];
-        while(i<=mid && j<=e){
-            if(arr[i]>arr[j]){
-                merge[k]=arr[i];
+
+class mergeSort {
+    void logicMerge(int arr[], int s, int mid, int e) {
+        mid = s + (e - s) / 2;
+        int i = 0, j = mid + 1, k = 0;
+        int merge[] = new int[e - s + 1];
+        while (i <= mid && j <= e) {
+            if (arr[i] > arr[j]) {
+                merge[k] = arr[i];
                 k++;
                 i++;
-            }
-            else{
-                merge[k]=arr[j];
+            } else {
+                merge[k] = arr[j];
                 k++;
                 j++;
             }
         }
-        while(i<=mid){
-            merge[k]=arr[i];
-            k++;i++;
+        while (i <= mid) {
+            merge[k] = arr[i];
+            k++;
+            i++;
         }
 
-        while(j<=e){
-            merge[k]=arr[j];
-            k++;j++;
+        while (j <= e) {
+            merge[k] = arr[j];
+            k++;
+            j++;
         }
-        for(i=0,j=s;i<merge.length;i++){
-            
+        for (i = 0, j = s; i < merge.length; i++) {
+            merge[k] = arr[i];
         }
+    }
 
+    void mergeSorting(int arr[], int s, int e) {
+        if (s < e) {
+            int mid = s + (e - s) / 2;
+            mergeSorting(arr, s, mid);
+            mergeSorting(arr, mid + 1, e);
+            logicMerge(null, s, mid, e);
+        }
     }
 }
+
 public class MergeSort {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        mergeSort ob = new mergeSort();
+        System.out.println("Enter the  Size of  the Array");
+        int size = sc.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Enter The Number In the Array");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ob.mergeSorting(arr, 0, size-1);
+        System.out.println("Array After Sort");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
 }
