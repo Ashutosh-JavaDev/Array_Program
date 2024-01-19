b6701955476e1335cf47b62bd2bf5a186ef80312
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 00:16:30 +0530

    Fyle Web Dev

---
+++ DifferentSorting/MergeSort.java
@@ -0,0 +1,17 @@
+package Array_Program.DifferentSorting;
+import java.util.Scanner;
+class mergeSort{
+    void logicMerge(int arr[],int s,int mid,int e){
+        mid=s+(e-s)/2;
+        int i=0,j=mid+1,k;
+        int merge[]=new int[e-s+1];
+        while(i<=mid && j<=e){
+            if(arr[i]>arr[j]){
+                
+            }
+        }
+    }
+}
+public class MergeSort {
+    
+}
