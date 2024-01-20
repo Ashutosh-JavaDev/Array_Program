79a08636e548e1aa5642cb98477b754fc4cc28e4
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 10:12:08 +0530

    Sorting Problem

---
+++ DifferentSorting/mergeSort.java
@@ -0,0 +1,18 @@
+package Array_Program.DifferentSorting;
+
+import java.util.Scanner;
+
+class merging {
+    void mErge(int arr[], int s, int e) {
+        int mid = s + (e - s) / 2;
+        int mergeArray[] = new int[e - s + 1];
+        int i = 0, j = mid + 1, k = 0;
+        while (i <= mid && j <= e) {
+
+        }
+    }
+}
+
+public class mergeSort {
+
+}
