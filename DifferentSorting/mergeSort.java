cbfa5c8c38b7506a2bcd1442f13a4aaaf9a7b35e
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 10:16:00 +0530

    Sorting Problem

--- DifferentSorting/mergeSort.java
+++ DifferentSorting/mergeSort.java
@@ -8,7 +8,26 @@ class merging {
         int mergeArray[] = new int[e - s + 1];
         int i = 0, j = mid + 1, k = 0;
         while (i <= mid && j <= e) {
-
+            if(arr[i]>arr[j]){
+                mergeArray[k]=arr[i];
+                i++;
+                k++;
+            }
+            else{
+                mergeArray[k]=arr[j];
+                k++;
+                j++;
+            }
+        }
+        while(i<=mid){
+            mergeArray[k]=arr[i];
+            k++;
+            i++;
+        }
+        while(j<=e){
+            mergeArray[k]=arr[j];
+            j++;
+            k++;
         }
     }
 }
