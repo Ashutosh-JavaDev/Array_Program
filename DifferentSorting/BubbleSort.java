c74377184951e625ac6aa899fe62bad4ff42ab94
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-20 11:40:21 +0530

    Sorting Problem

--- DifferentSorting/BubbleSort.java
+++ DifferentSorting/BubbleSort.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 class BubSort{
     void bubsort(int arr[],int size){
         for(int i=0;i<size-1;i++){
-            for(int j=i+1;j<size-i-1;j++){
-                if(arr[i]<arr[j]){
+            for(int j=0;j<size-i-1;j++){
+                if(arr[i]<arr[i+1]){
                     int temp=arr[i];
                     arr[i]=arr[i+1];
                     arr[i+1]=temp;
@@ -19,5 +19,17 @@ class BubSort{
     }
 }
 public class BubbleSort {
-    
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        BubSort ob=new BubSort();
+        System.out.println("Enter the Size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter The VAlue in the Array");
+        for(int i=0;i<size;i++){
+            arr[i]=sc.nextInt();
+        }
+        ob.bubsort(arr, size);
+
+    }
 }
