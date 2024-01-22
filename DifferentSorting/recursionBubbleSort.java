c3835b9ff8431cd55a758ecb0e25712d2337c466
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-22 22:01:07 +0530

    Recursion

--- DifferentSorting/recursionBubbleSort.java
+++ DifferentSorting/recursionBubbleSort.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 class sorted {
     void makeArraySort(int arr[], int i, int j, int size) {
-        if (i >= size - 1) {
+        if (i >= size - 1|| size==1) {
             return;
         } else if (j < size - i - 1) {
             if (arr[j] > arr[j + 1]) {
 
                 int temp = arr[j];
-                arr[i] = arr[j + 1];
+                arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
             makeArraySort(arr, i, j + 1, size);
 
         } else {
-            makeArraySort(arr, i + 1, j, size);
+            makeArraySort(arr, i + 1, 0, size);
         }
     }
 }
@@ -25,10 +25,10 @@ public class recursionBubbleSort {
     public static void main(String[] args) {
         sorted ob = new sorted();
         int arr[] = { 3, 5, 1, 2, 6, 21, 12 };
-        ob.makeArraySort(arr, 0, 0, arr.length-1);
+        ob.makeArraySort(arr, 0, 0, arr.length);
         System.out.println("Array After Sort");
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
-        }
+       for(int element:arr){
+        System.out.print(element+" ");
+       }
     }
 }
