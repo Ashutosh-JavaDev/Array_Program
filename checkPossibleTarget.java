772d5e0776646745172ce3c0274659eb94ef5a76
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-25 17:22:14 +0530

    Program

--- checkPossibleTarget.java
+++ checkPossibleTarget.java
@@ -6,12 +6,12 @@ class SumTarget{
        if(target>0){
         for(int i=0;i<size;i++){
             for(int j=i+1;j<size;j++){
-                // if(arr[i]!=arr[j] && i!=j){
-                   if(arr[i]+arr[j]==target){
-                    result=true;
-                    System.out.println("arr["+i+"]+arr["+j+"]="+target);
-                   }
-                // }
+                if(arr[i]!=arr[j] && i!=j){
+                    if(arr[i]+arr[j]==target){
+                        result=true;
+                    System.out.println(arr[i]+"+"+arr[j]+target);
+                    }
+                }
             }
         }
        }
@@ -30,7 +30,7 @@ public class checkPossibleTarget {
     for(int i=0;i<size;i++){
         arr[i]=sc.nextInt();
     }    
-    boolean res=ob.targetSum(arr, 10, size);
+    boolean res=ob.targetSum(arr, 7, size);
     System.out.println("Result: "+res);
 
  }   
