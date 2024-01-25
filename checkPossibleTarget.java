f40f386f70ffc688129743832e5604946c4b3b30
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-25 16:59:49 +0530

    Program

--- checkPossibleTarget.java
+++ checkPossibleTarget.java
@@ -6,7 +6,9 @@ class SumTarget{
        if(target<0){
         for(int i=0;i<size;i++){
             for(int j=i+1;j<size;j++){
-                
+                if(arr[i]!=arr[j] && i!=j){
+                    System.out.println("arr["+i+"]+arr["+j+"]="+arr[i]+arr[j]);
+                }
             }
         }
        }
