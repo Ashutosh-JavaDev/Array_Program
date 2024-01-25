fcf5448d5f67b889e4ac0a91ed1c12cbe5649a9b
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-25 16:56:46 +0530

    Program

--- checkPossibleTarget.java
+++ checkPossibleTarget.java
@@ -1,7 +1,18 @@
 package Array_Program;
 import java.util.Scanner;
 class SumTarget{
-    
+    boolean targetSum(int arr[],int target,int size){
+        boolean result=false;
+       if(target<0){
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                
+            }
+        }
+       }
+        return result;
+
+    }
 }public class checkPossibleTarget {
     
 }
