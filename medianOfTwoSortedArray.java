ce7945f8a0352cc18e4b32bc9fd6838035b55a46
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 00:02:09 +0530

    Program

--- medianOfTwoSortedArray.java
+++ medianOfTwoSortedArray.java
@@ -1,8 +1,22 @@
 package Array_Program;
 class median{
-    // int mediansorted(int arr1[],int size1,int arr2[]){
-    //     if()
-    // }
+    float mediansorted(int arr1[],int size1,int size2,int arr2[]){
+        int total=size1+size2;
+        if(total%2!=0){
+            int sum=0;
+            for(int i=0;i<total;i++){
+                sum+=i;
+            }
+            float Average=sum/total;
+            return Average;
+        }
+        else{
+            int s=0,e=total-1;
+            int mid=s+(e-s)/2;
+            float median=(arr1[mid]+arr2[mid+1]);
+            return median;
+        }
+    }
 }
 public class medianOfTwoSortedArray {
     
