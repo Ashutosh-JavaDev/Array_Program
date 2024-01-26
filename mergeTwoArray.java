a41235cb11586916367fa69a9bd182e54dae7678
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 15:11:59 +0530

    Program

--- mergeTwoArray.java
+++ mergeTwoArray.java
@@ -2,6 +2,8 @@ package Array_Program;
 import java.util.Scanner;
 class twoMergeArray{
     void mergeTwoArray(int arr1[],int arr2[],int size1,int size2,int arr3[]){
+        int total=size1+size2;
+        arr3=new int[total];
         
     }
 }
