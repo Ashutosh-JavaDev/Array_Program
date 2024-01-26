3eb335e0421d1408a3b9d6d8ac02cbebfc889700
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 15:30:33 +0530

    Program

--- mergeTwoArray.java
+++ mergeTwoArray.java
@@ -33,8 +33,17 @@ public class mergeTwoArray {
         int size2=sc.nextInt();
         int total=(size1+size2)-1;
         int arr1[]=new int [size1];
+        System.out.println("Enter Elements in Array 1");
+        for(int i=0;i<size1;i++){
+            arr1[i]=sc.nextInt();
+        }
         int arr2[]=new int [size2];
+        System.out.println("Enter Elements in Array 2");
+        for(int i=0;i<size1;i++){
+            arr2[i]=sc.nextInt();
+        }
         int arr3[]=new int[total];
+       
         ob.mergeTwoArray(arr1, arr2, size1, size2, arr3);
     }
 }
