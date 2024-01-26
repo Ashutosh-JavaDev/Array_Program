f084c2930e9602f701cfd7479faf4d26ae797fd6
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 15:39:55 +0530

    Program

--- mergeTwoArray.java
+++ mergeTwoArray.java
@@ -1,49 +1,58 @@
 package Array_Program;
+
 import java.util.Scanner;
-class twoMergeArray{
-    void mergeTwoArray(int arr1[],int arr2[],int size1,int size2,int arr3[]){
-        int total=size1+size2;
-        arr3=new int[total];
-        int k=0;
-        for(int i=0;i<size1;i++){
-            for(int j=0;j<size2;j++){
-                if(i<=size1){
-                    arr3[k]=arr1[i];
-                    k++;
-                }
-                else{
-                    arr3[k]=arr2[j];
-                    k++;
-                }
-            }
+
+class twoMergeArray {
+    void mergeTwoArray(int arr1[], int arr2[], int size1, int size2, int arr3[]) {
+        int total = size1 + size2;
+        arr3 = new int[total];
+        int k = 0;
+        for (int i = 0; i < size1; i++) {
+            arr3[k] = arr1[i];
+            k++;
         }
-        System.out.println("Third Array Value");
-        for(int i=0;i<total;i++){
-            System.out.println(arr3[i]);
+        for (int j = 0; j < size2; j++) {
+            arr3[k] = arr2[j];
+            k++;
+        }
+
+        // System.out.println("Third Array Value");
+        // for (int i = 0; i < total; i++) {
+        // System.out.println(arr3[i]);
+        // }
+        int sum = 0;
+        float avg = 0;
+        if (total % 2 != 0) {
+            for (int i = 0; i < total; i++) {
+                sum = sum + arr3[i];
+            }
+
+            avg = sum / total;
         }
     }
 }
+
 public class mergeTwoArray {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        twoMergeArray ob=new twoMergeArray();
+        Scanner sc = new Scanner(System.in);
+        twoMergeArray ob = new twoMergeArray();
         System.out.println("Enter Size of the Array 1");
-        int size1=sc.nextInt();
+        int size1 = sc.nextInt();
         System.out.println("Enter the Size of the Second Array");
-        int size2=sc.nextInt();
-        int total=(size1+size2)-1;
-        int arr1[]=new int [size1];
+        int size2 = sc.nextInt();
+        int total = size1 + size2;
+        int arr1[] = new int[size1];
         System.out.println("Enter Elements in Array 1");
-        for(int i=0;i<size1;i++){
-            arr1[i]=sc.nextInt();
+        for (int i = 0; i < size1; i++) {
+            arr1[i] = sc.nextInt();
         }
-        int arr2[]=new int [size2];
+        int arr2[] = new int[size2];
         System.out.println("Enter Elements in Array 2");
-        for(int i=0;i<size1;i++){
-            arr2[i]=sc.nextInt();
+        for (int i = 0; i < size2; i++) {
+            arr2[i] = sc.nextInt();
         }
-        int arr3[]=new int[total];
-       
+        int arr3[] = new int[total];
+
         ob.mergeTwoArray(arr1, arr2, size1, size2, arr3);
     }
 }
