846faf8788c63b6b0d6ec6f6a5fd3f7e3447d00a
Ashutosh Ayush <jbnashu297@gmail.com>
2024-01-26 15:13:37 +0530

    Program

--- mergeTwoArray.java
+++ mergeTwoArray.java
@@ -4,7 +4,19 @@ class twoMergeArray{
     void mergeTwoArray(int arr1[],int arr2[],int size1,int size2,int arr3[]){
         int total=size1+size2;
         arr3=new int[total];
-        
+        int k=0;
+        for(int i=0;i<size1;i++){
+            for(int j=0;j<size2;j++){
+                if(i<=size1){
+                    arr3[k]=arr1[i];
+                    k++;
+                }
+                else{
+                    arr3[k]=arr2[j];
+                    k++;
+                }
+            }
+        }
     }
 }
 public class mergeTwoArray {
