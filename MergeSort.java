b2af6dc8aad052a9e662d00ac4d355f3e71af84c
Ashutosh Ayush <jbnashu297@gmail.com>
2024-02-28 15:00:52 +0530

    Project

--- MergeSort.java
+++ MergeSort.java
@@ -2,7 +2,18 @@ package Array_Program;
 import java .util.Scanner;
 class merge{
     void sorting(int arr[],int s,int mid,int e){
-        
+        int arr3[]=new int[s+e];
+        mid=s+(e-s)/2;
+        int i=s,j=mid+1,k=s;
+        while(i<=mid&&j<=e){
+            if(arr[i]>arr[j]){
+                arr3[k++]=arr[i++];
+            }
+            else{
+                arr3[k++]=arr[j++];
+            }
+        }
+
     }
 }
 public class MergeSort {
