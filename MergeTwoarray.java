c2d46fe009033fa6408d4ad997d69623d238436c
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-05 15:31:19 +0530

    three sum

--- MergeTwoarray.java
+++ MergeTwoarray.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 class mergenumber {
     public float mergetwo(int arr1[], int size1, int arr2[], int size2) {
         int size = arr1.length + arr2.length;
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
+      
 
         int newArr[] = new int[size];
         int i = 0, j = 0, l = 0;
@@ -21,17 +20,16 @@ class mergenumber {
             j++;
         }
         Arrays.sort(newArr);
-        System.out.println(size);
-        for(int k=0;k<size;k++){
-            System.out.print(newArr[k]+" ");
-        }
-        System.out.println();
         float mid = 0;
         if (size % 2 != 0) {
             mid = newArr[size / 2];
         }
         else{
            int temp=size/2;
+           temp-=1;
+         
+           System.out.print(newArr[temp]+" "+newArr[temp+1] );
+
             mid=newArr[size/2]+newArr[temp+1];
             mid/=2;
         }
@@ -42,8 +40,8 @@ class mergenumber {
 public class MergeTwoarray {
     public static void main(String[] args) {
         mergenumber m = new mergenumber();
-        int arr1[] = { 0,0,0,0,0 };
-        int arr2[] = { -1,0,0,0,0,0,1 };
+        int arr1[] = {1,2 };
+        int arr2[] = { 3,4 };
         float mid = m.mergetwo(arr1, arr1.length, arr2, arr2.length);
         System.out.println(mid);
     }
