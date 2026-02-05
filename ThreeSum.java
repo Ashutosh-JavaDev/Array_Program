998f8f76c810a9ba7e89aeef9091309ad22133a3
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-05 15:36:15 +0530

    three sum

---
+++ ThreeSum.java
@@ -0,0 +1,16 @@
+package Array_Program;
+import java.util.ArrayList;
+import java.util.List;
+class threeSum{
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        List<List<Integer>> result = new ArrayList<>();
+
+        
+
+        return result; 
+    }
+}
+public class ThreeSum {
+    
+}
