abe4f251b3ac8f43744e8902eae97edf9c17e726
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-05 11:58:46 +0530

    merge two array

--- MergeTwoarray.java
+++ MergeTwoarray.java
@@ -1,4 +1,31 @@
 package Array_Program;
-public class MergeTwoarray{
 
+class merge {
+    public void mergetwo(int arr1[], int size1, int arr2[], int size2) {
+        int size = arr1.length + arr2.length;
+        int newArr[] = new int[size];
+        int i = 0, j = 0;
+        while (i < arr1.length) {
+            newArr[j] = arr1[i];
+            i++;
+            j++;
+        }
+        while (i < arr2.length) {
+            newArr[j] = arr2[i];
+            i++;
+            j++;
+        }
+        for(int k=0;k<size;k++){
+            System.out.print(newArr[k]+ " ");
+        }
+    }
+}
+
+public class MergeTwoarray {
+   public static void main(String[] args) {
+    merge m=new merge();
+    int arr1[]={2,3,5,6};
+    int arr2[]={1,4,7,8};
+    m.mergetwo(arr1,arr1.length,arr2,arr2.length);
+   }
 }
